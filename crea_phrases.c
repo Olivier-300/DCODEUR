c12b23e3ee87e6a469266b533c49a12e1f60052a
Yann <yann.tran@efrei.net>
2022-11-11 18:56:01 +0100

    fonction crea_phrase

--- crea_phrases.c
+++ crea_phrases.c
@@ -1,5 +1,47 @@
 //
 // Created by olivi on 27/10/2022.
 //
-
+#include "ARBRES_VERBE.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "crea_phrases.h"
+int is_forme_flechie_in_node(noeud_verbe* n)
+{
+    if (n->nb_formes)
+        return 1;
+    return 0;
+}
+int count_sons_in_node(noeud_verbe* n)
+{
+    int cpt=0;
+    while(n->sons[cpt]!=NULL)
+        cpt++;
+    if (cpt==0)
+        return 0;
+    return cpt;
+
+}
+
+void verb_random_node(noeud_verbe* n )
+{
+    int next = 1;
+    int i = rand() % count_sons_in_node(n);
+
+    while (count_sons_in_node(n) && next == 1)
+    {
+        n=n->sons[i];
+        printf("%c",n->lettre);
+        i = rand() % count_sons_in_node(n);
+        if (is_forme_flechie_in_node(n))
+        {
+            next = rand() % 2;
+        }
+    }
+
+}
+void verb_random_tree(a_vrb* t )
+{
+    verb_random_node(t->root);
+}
+
--- crea_phrases.h
+++ crea_phrases.h
@@ -4,5 +4,8 @@
 
 #ifndef DCODEUR_CREA_PHRASES_H
 #define DCODEUR_CREA_PHRASES_H
-
+int is_forme_flechie_in_node(noeud_verbe*);
+int count_sons_in_node(noeud_verbe*);
+void verb_random_node(noeud_verbe*);
+void verb_random_tree(a_vrb*);
 #endif //DCODEUR_CREA_PHRASES_H
